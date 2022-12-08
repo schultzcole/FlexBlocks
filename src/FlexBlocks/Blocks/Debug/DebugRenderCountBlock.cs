7ad7e897805e5ed981aa033fe33b557a4f97683e
Cole Schultz <schultzcole@gmail.com>
2022-12-07 22:40:31 -0600

    add debug block that shows the # of times that it has been rendered

---
+++ src/FlexBlocks/Blocks/Debug/DebugRenderCountBlock.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.HighPerformance;
+
+namespace FlexBlocks.Blocks.Debug;
+
+public class DebugRenderCountBlock : UiBlock
+{
+    private int _renderCount;
+
+    public required UiBlock Content { get; init; }
+
+    public override bool ShouldRerenderWithChildren => true;
+
+    public override BlockSize CalcDesiredSize(BlockSize maxSize) => Content.CalcDesiredSize(maxSize);
+
+    public override void Render(Span2D<char> buffer)
+    {
+        RenderChild(Content, buffer);
+
+        _renderCount++;
+        _renderCount.ToString().CopyTo(buffer.GetRowSpan(0));
+    }
+}
