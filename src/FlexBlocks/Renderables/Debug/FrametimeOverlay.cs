29144a08a598a6acf0bf06f4bf0e9542b9cbc49e
Cole Schultz <schultzcole@gmail.com>
2022-12-11 20:26:05 -0600

    add frametime overlay, which shows the time elapsed since the last frame

---
+++ src/FlexBlocks/Renderables/Debug/FrametimeOverlay.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.HighPerformance;
+using FlexBlocks.Blocks;
+
+namespace FlexBlocks.Renderables.Debug;
+
+public sealed class FrametimeOverlay : IRenderable
+{
+    public bool ShowFps { get; set; }
+
+    private long? _prevTimeTicks;
+
+    /// <inheritdoc />
+    public void Render(Span2D<char> buffer)
+    {
+        var currentTimeTicks = DateTime.Now.Ticks;
+        TimeSpan? delta = _prevTimeTicks is not null ? TimeSpan.FromTicks(currentTimeTicks - _prevTimeTicks.Value) : null;
+        _prevTimeTicks = currentTimeTicks;
+
+        if (delta is not { } dt) return;
+
+        var str = ShowFps
+            ? $"{dt.TotalMilliseconds:F2}ms ({(int)(1 / dt.TotalSeconds)})"
+            : $"{dt.TotalMilliseconds:F2}ms";
+
+        if (str.Length > buffer.Width) return;
+
+        var strBuffer = buffer.GetRowSpan(0)[(buffer.Width - str.Length)..];
+        str.CopyTo(strBuffer);
+    }
+}
