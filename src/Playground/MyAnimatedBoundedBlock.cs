e0bed02484b4e1892be70a671ace775df9c561fc
Cole Schultz <schultzcole@gmail.com>
2022-12-13 21:24:41 -0600

    update playground demo with size animation for testing purposes

---
+++ src/Playground/MyAnimatedBoundedBlock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.HighPerformance;
+using FlexBlocks;
+using FlexBlocks.BlockProperties;
+using FlexBlocks.Blocks;
+
+namespace Playground;
+
+/// A custom implementation of BoundedBlock that re-renders itself periodically
+/// Disposing this Block stops the re-render loop.
+public class MyAnimatedBoundedBlock : BoundedBlock, IDisposable
+{
+    private Task? _rerenderTask;
+    private readonly CancellationTokenSource _cts = new();
+
+    public override void Render(Span2D<char> buffer)
+    {
+        base.Render(buffer);
+
+        RegisterRerenderTask();
+    }
+
+    private int _expandDir = 1;
+
+    private void RegisterRerenderTask()
+    {
+        if (_rerenderTask is null && !_cts.Token.IsCancellationRequested)
+        {
+            _rerenderTask = Task.Run(async () =>
+                {
+                    while (!_cts.Token.IsCancellationRequested)
+                    {
+                        await Task.Delay(10, _cts.Token);
+
+                        if (!CanRequestRerender) continue;
+
+                        if (Width != BlockLength.Unbounded)
+                        {
+                            _expandDir = Width.Value switch
+                            {
+                                >= 80 => -1,
+                                <= 15 => 1,
+                                _     => _expandDir
+                            };
+
+                            Width += _expandDir;
+                        }
+
+                        RequestRerender(RerenderMode.DesiredSizeChanged);
+                    }
+                }
+            );
+        }
+    }
+
+    public void Dispose() { _cts.Dispose(); }
+}
--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using FlexBlocks;
+using FlexBlocks.BlockProperties;
 using FlexBlocks.Blocks;
+using FlexBlocks.Blocks.Debug;
+using FlexBlocks.Renderables;
+using FlexBlocks.Renderables.Debug;
 using Playground;
 
 var block = new BorderBlock
 {
-    Border = Border.Square,
-    // Padding = new Padding(0, 1),
+    Overlay = new CompositeRenderable
+    {
+        new DimensionsOverlay(),
+        new RenderCountOverlay(),
+        new FrametimeOverlay { ShowFps = true },
+    },
+    Border = Borders.Square,
+    Padding = new Padding(2, 5),
     HorizontalSizing = Sizing.Fill,
     VerticalSizing = Sizing.Fill,
-    // HorizontalContentAlignment = Alignment.Center,
-    // VerticalContentAlignment = Alignment.Center,
-    Content = new MyCustomRandomStringBlock
+    Content = new BorderBlock
     {
-        Width = 10,
-        Height = 10
-    }
+        Overlay = new CompositeRenderable
+        {
+            new DimensionsOverlay(),
+            new RenderCountOverlay(),
+        },
+        Border = Borders.Square,
+        Padding = new Padding(1, 3),
+        HorizontalSizing = Sizing.Content,
+        VerticalSizing = Sizing.Fill,
+        Content = new MyAnimatedBoundedBlock
+        {
+            DesiredSize = DesiredBlockSize.From(11, 15),
+            Content = new RandomStringBlock(),
+        },
+    },
 };
 
-await FlexBlocksDriver.Run(block);
+Console.Title = "FlexBlocks Playground";
 
+Console.WriteLine("Hello, World!");
+await FlexBlocksDriver.Run(block);
 Console.WriteLine("Goodbye!");
