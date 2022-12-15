8ef97d50db4444b64551d193114b04da26bf5dc6
Cole Schultz <schultzcole@gmail.com>
2022-12-14 18:19:55 -0600

    remove double math and comparison operators form BlockLength

--- src/FlexBlocks/BlockProperties/BlockLength.cs
+++ src/FlexBlocks/BlockProperties/BlockLength.cs
@@ -4,24 +4,18 @@ namespace FlexBlocks.BlockProperties;
 
 /// <summary>Represents a potentially unbounded length in a single dimension.</summary>
 public record struct BlockLength :
-    IAdditionOperators<BlockLength, int?, BlockLength>,
-    IAdditionOperators<BlockLength, double?, BlockLength>,
     IAdditionOperators<BlockLength, BlockLength, BlockLength>,
-    ISubtractionOperators<BlockLength, int?, BlockLength>,
-    ISubtractionOperators<BlockLength, double?, BlockLength>,
     ISubtractionOperators<BlockLength, BlockLength, BlockLength>,
-    IMultiplyOperators<BlockLength, int?, BlockLength>,
-    IMultiplyOperators<BlockLength, double?, BlockLength>,
     IMultiplyOperators<BlockLength, BlockLength, BlockLength>,
-    IDivisionOperators<BlockLength, int?, BlockLength>,
-    IDivisionOperators<BlockLength, double?, BlockLength>,
     IDivisionOperators<BlockLength, BlockLength, BlockLength>,
-    IModulusOperators<BlockLength, int?, BlockLength>,
-    IModulusOperators<BlockLength, double?, BlockLength>,
     IModulusOperators<BlockLength, BlockLength, BlockLength>,
     IComparisonOperators<BlockLength, BlockLength, bool>,
-    IComparisonOperators<BlockLength, int?, bool>,
-    IComparisonOperators<BlockLength, double?, bool>
+    IAdditionOperators<BlockLength, int?, BlockLength>,
+    ISubtractionOperators<BlockLength, int?, BlockLength>,
+    IMultiplyOperators<BlockLength, int?, BlockLength>,
+    IDivisionOperators<BlockLength, int?, BlockLength>,
+    IModulusOperators<BlockLength, int?, BlockLength>,
+    IComparisonOperators<BlockLength, int?, bool>
 {
     public int? Value { get; private init; }
 
@@ -66,21 +60,6 @@ public record struct BlockLength :
     /// <summary>Moduluses a length by a nullable int. If the length is unbounded or if the value is null, the result will be unbounded</summary>
     public static BlockLength operator %(BlockLength length, int? value) => From(length.Value % value);
 
-    /// <summary>Adds a length with a nullable double. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator +(BlockLength length, double? value) => From((int?)(length.Value + value));
-
-    /// <summary>Subtracts a nullable double from a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator -(BlockLength length, double? value) => From((int?)(length.Value - value));
-
-    /// <summary>Divides a length by a nullable double. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator /(BlockLength length, double? value) => From((int?)(length.Value / value));
-
-    /// <summary>Multiplies a length with a nullable double. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator *(BlockLength length, double? value) => From((int?)(length.Value * value));
-
-    /// <summary>Moduluses a length by a nullable double. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator %(BlockLength length, double? value) => From((int?)(length.Value % value));
-
     /// <summary>Adds a nullable int with a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
     public static BlockLength operator +(int? value, BlockLength length) => From(length.Value + value);
 
@@ -95,21 +74,6 @@ public record struct BlockLength :
 
     /// <summary>Moduluses a nullable int by a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
     public static BlockLength operator %(int? value, BlockLength length) => From(length.Value % value);
-
-    /// <summary>Adds a nullable double with a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator +(double? value, BlockLength length) => From((int?)(length.Value + value));
-
-    /// <summary>Subtracts a length from a nullable double. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator -(double? value, BlockLength length) => From((int?)(length.Value - value));
-
-    /// <summary>Divides a nullable double by a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator /(double? value, BlockLength length) => From((int?)(length.Value / value));
-
-    /// <summary>Multiplies a nullable double with a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator *(double? value, BlockLength length) => From((int?)(length.Value * value));
-
-    /// <summary>Moduluses a nullable double by a length. If the length is unbounded or if the value is null, the result will be unbounded</summary>
-    public static BlockLength operator %(double? value, BlockLength length) => From((int?)(length.Value % value));
 
     /// <inheritdoc />
     public static bool operator >(BlockLength left, BlockLength right) =>
@@ -149,30 +113,6 @@ public record struct BlockLength :
 
     /// <inheritdoc />
     public static bool operator <=(BlockLength left, int? right) =>
-        UnboundedNumberComparisons.LessThanOrEqual(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator ==(BlockLength left, double? right) =>
-        UnboundedNumberComparisons.Equal(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator !=(BlockLength left, double? right) =>
-        UnboundedNumberComparisons.NotEqual(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator >(BlockLength left, double? right) =>
-        UnboundedNumberComparisons.GreaterThan(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator >=(BlockLength left, double? right) =>
-        UnboundedNumberComparisons.GreaterThanOrEqual(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator <(BlockLength left, double? right) =>
-        UnboundedNumberComparisons.LessThan(left.Value, right);
-
-    /// <inheritdoc />
-    public static bool operator <=(BlockLength left, double? right) =>
         UnboundedNumberComparisons.LessThanOrEqual(left.Value, right);
 }
 
