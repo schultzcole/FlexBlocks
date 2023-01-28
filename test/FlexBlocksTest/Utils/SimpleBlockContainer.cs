a5d22cb85d92db7da01ff3594ebbdbbd51c773cb
Cole Schultz <schultzcole@gmail.com>
2023-01-28 14:24:19 -0600

    add a very simple IBlockContainer implementation for tests

---
+++ test/FlexBlocksTest/Utils/SimpleBlockContainer.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.HighPerformance;
+using FlexBlocks;
+using FlexBlocks.Blocks;
+
+namespace FlexBlocksTest.Utils;
+
+public class SimpleBlockContainer : IBlockContainer
+{
+    public void RenderBlock(UiBlock block, Span2D<char> buffer)
+    {
+        block.Container = this;
+        block.Background?.Render(buffer);
+        block.Render(buffer);
+        block.Overlay?.Render(buffer);
+    }
+
+    /// <inheritdoc />
+    public void RenderChild(UiBlock parent, UiBlock child, Span2D<char> childBuffer) => RenderBlock(child, childBuffer);
+
+    /// <inheritdoc />
+    public void RequestRerender(UiBlock block, RerenderMode rerenderMode = RerenderMode.InPlace) =>
+        throw new System.NotImplementedException();
+}
