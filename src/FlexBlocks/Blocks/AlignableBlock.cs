040878c80fa5bd0d876ea672252d4bdd21cd87e4
Cole Schultz <schultzcole@gmail.com>
2023-01-28 15:23:02 -0600

    Extract abstract "AlignableBlock" from content block
    this will allow the alignment/sizing code to be shared by blocks that have different content, like flexblock and gridblock

---
+++ src/FlexBlocks/Blocks/AlignableBlock.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using CommunityToolkit.HighPerformance;
+using FlexBlocks.BlockProperties;
+
+namespace FlexBlocks.Blocks;
+
+/// <summary>
+/// An abstract block that sizes itself and aligns content within itself based on its content.
+/// The exact nature of the content is determined by the subclass.
+/// </summary>
+public abstract class AlignableBlock : UiBlock
+{
+    public Alignment HorizontalContentAlignment { get; set; } = Alignment.Start;
+    public Alignment VerticalContentAlignment { get; set; } = Alignment.Start;
+
+    public Sizing HorizontalSizing { get; set; } = Sizing.Content;
+    public Sizing VerticalSizing { get; set; } = Sizing.Content;
+
+    /// <inheritdoc />
+    public override RerenderMode GetRerenderModeForChild(UiBlock child)
+    {
+        // If this block is sized according to its child in either dimension, our size might change
+        return HorizontalSizing == Sizing.Content || VerticalSizing == Sizing.Content
+            ? RerenderMode.DesiredSizeChanged
+            : RerenderMode.InPlace;
+    }
+
+    /// <summary>Whether this alignable block has any content to align.</summary>
+    public abstract bool HasContent { get; }
+
+    /// <summary>Computes the max size of the content contained by this.</summary>
+    protected abstract UnboundedBlockSize? CalcContentMaxSize();
+
+    /// <inheritdoc />
+    public override UnboundedBlockSize CalcMaxSize()
+    {
+        if (HorizontalSizing == Sizing.Fill && VerticalSizing == Sizing.Fill)
+        {
+            return UnboundedBlockSize.Unbounded;
+        }
+
+        var contentSize = CalcContentMaxSize() ?? UnboundedBlockSize.From(0, 0);
+
+        return (HorizontalSizing, VerticalSizing) switch
+        {
+            (Sizing.Content, Sizing.Content) => contentSize,
+            (Sizing.Content, Sizing.Fill)    => contentSize with { Height = BlockLength.Unbounded },
+            (Sizing.Fill, Sizing.Content)    => contentSize with { Width = BlockLength.Unbounded },
+
+            // (Fill, Fill) case is covered above
+            _ => throw new UnreachableException($"Unknown sizing values. H={HorizontalSizing}, V={VerticalSizing}")
+        };
+    }
+
+    /// <summary>Computes the size of this block's contents given a concrete maximum size.</summary>
+    protected abstract BlockSize? CalcContentSize(BlockSize maxSize);
+
+    /// <inheritdoc />
+    public override BlockSize CalcSize(BlockSize maxSize)
+    {
+        if (HorizontalSizing == Sizing.Fill && VerticalSizing == Sizing.Fill)
+        {
+            return maxSize;
+        }
+
+        var contentSize = CalcContentSize(maxSize) ?? BlockSize.From(0, 0);
+
+        return (HorizontalSizing, VerticalSizing) switch
+        {
+            (Sizing.Content, Sizing.Content) => contentSize,
+            (Sizing.Content, Sizing.Fill)    => contentSize with { Height = maxSize.Height },
+            (Sizing.Fill, Sizing.Content)    => contentSize with { Width = maxSize.Width },
+
+            // (Fill, Fill) case is covered above
+            _ => throw new UnreachableException($"Unknown sizing values. H={HorizontalSizing}, V={VerticalSizing}")
+        };
+    }
+
+    /// <summary>Determines what subset of the total content buffer this block's content should be rendered to,
+    /// according to the alignment settings.</summary>
+    protected Span2D<char> ComputeAlignedContentBuffer(Span2D<char> buffer)
+    {
+        if (!HasContent) return buffer;
+
+        var maxSize = buffer.BlockSize();
+        var contentSize = CalcContentSize(maxSize)!.Value.Constrain(maxSize);
+
+        var hDimension = ComputeAlignedDimension(maxSize.Width, contentSize.Width, HorizontalContentAlignment);
+        var vDimension = ComputeAlignedDimension(maxSize.Height, contentSize.Height, VerticalContentAlignment);
+
+        return buffer.Slice(
+            row: vDimension.start,
+            column: hDimension.start,
+            height: vDimension.length,
+            width: hDimension.length
+        );
+    }
+
+    /// <summary>Calculates a slice of the full length of a single dimension that the content should be rendered to.</summary>
+    private static (int start, int length) ComputeAlignedDimension(int maxSize, int desiredSize, Alignment alignment)
+    {
+        var sanitizedSize = Math.Min(maxSize, desiredSize);
+
+        switch (alignment)
+        {
+            case Alignment.Start: return (0, sanitizedSize);
+            case Alignment.Center:
+                var margin = (maxSize - sanitizedSize) / 2;
+                var length = sanitizedSize;
+                return (margin, length);
+            case Alignment.End: return (maxSize - sanitizedSize, sanitizedSize);
+            default: throw new ArgumentOutOfRangeException(nameof(alignment), alignment, "Unknown alignment type");
+        }
+    }
+}
--- src/FlexBlocks/Blocks/ContentBlock.cs
+++ src/FlexBlocks/Blocks/ContentBlock.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using CommunityToolkit.HighPerformance;
+﻿using CommunityToolkit.HighPerformance;
 using FlexBlocks.BlockProperties;
 
 namespace FlexBlocks.Blocks;
@@ -12,74 +11,21 @@ public enum Alignment { Start, Center, End }
 /// A block type that contains another single block, can align that block within itself,
 /// and can either size itself to its content or grow to fill available space in either or both dimensions.
 /// </summary>
-public class ContentBlock : UiBlock
+public class ContentBlock : AlignableBlock
 {
     public UiBlock? Content { get; set; }
 
-    public Alignment HorizontalContentAlignment { get; set; } = Alignment.Start;
-    public Alignment VerticalContentAlignment { get; set; } = Alignment.Start;
-
-    public Sizing HorizontalSizing { get; set; } = Sizing.Content;
-    public Sizing VerticalSizing { get; set; } = Sizing.Content;
-
-    /// <inheritdoc />
-    public override RerenderMode GetRerenderModeForChild(UiBlock child)
-    {
-        if (child != Content)
-        {
-            throw new Exception("This child isn't my child...");
-        }
-
-        // If this block is sized according to its child in either dimension, our size might change
-        return HorizontalSizing == Sizing.Content || VerticalSizing == Sizing.Content
-            ? RerenderMode.DesiredSizeChanged
-            : RerenderMode.InPlace;
-    }
-
-    /// <inheritdoc />
-    public override UnboundedBlockSize CalcMaxSize()
-    {
-        if (HorizontalSizing == Sizing.Fill && VerticalSizing == Sizing.Fill)
-        {
-            return UnboundedBlockSize.Unbounded;
-        }
-
-        var contentSize = Content?.CalcMaxSize() ?? UnboundedBlockSize.From(0, 0);
-
-        return (HorizontalSizing, VerticalSizing) switch
-        {
-            (Sizing.Content, Sizing.Content) => contentSize,
-            (Sizing.Content, Sizing.Fill)    => contentSize with { Height = BlockLength.Unbounded },
-            (Sizing.Fill, Sizing.Content)    => contentSize with { Width = BlockLength.Unbounded },
-
-            // (Fill, Fill) case is covered above
-            _ => throw new UnreachableException($"Unknown sizing values. H={HorizontalSizing}, V={VerticalSizing}")
-        };
-    }
-
-    /// <inheritdoc />
-    public override BlockSize CalcSize(BlockSize maxSize)
-    {
-        if (HorizontalSizing == Sizing.Fill && VerticalSizing == Sizing.Fill)
-        {
-            return maxSize;
-        }
-
-        var contentSize = Content?.CalcSize(maxSize) ?? BlockSize.From(0, 0);
-
-        return (HorizontalSizing, VerticalSizing) switch
-        {
-            (Sizing.Content, Sizing.Content) => contentSize,
-            (Sizing.Content, Sizing.Fill)    => contentSize with { Height = maxSize.Height },
-            (Sizing.Fill, Sizing.Content)    => contentSize with { Width = maxSize.Width },
-
-            // (Fill, Fill) case is covered above
-            _ => throw new UnreachableException($"Unknown sizing values. H={HorizontalSizing}, V={VerticalSizing}")
-        };
-    }
-
     /// <inheritdoc />
     public override void Render(Span2D<char> buffer) => RenderContent(buffer);
+
+    /// <inheritdoc />
+    public override bool HasContent => Content is not null;
+
+    /// <inheritdoc />
+    protected override UnboundedBlockSize? CalcContentMaxSize() => Content?.CalcMaxSize();
+
+    /// <inheritdoc />
+    protected override BlockSize? CalcContentSize(BlockSize maxSize) => Content?.CalcSize(maxSize);
 
     /// <summary>Renders this block's content to the given buffer, possibly aligned (<see cref="Alignment"/>).</summary>
     protected virtual void RenderContent(Span2D<char> contentBuffer)
@@ -89,42 +35,5 @@ public class ContentBlock : UiBlock
         var alignedBuffer = ComputeAlignedContentBuffer(contentBuffer);
 
         RenderChild(Content, alignedBuffer);
-    }
-
-    /// <summary>Determines what subset of the total content buffer this block's content should be rendered to,
-    /// according to the alignment settings.</summary>
-    protected Span2D<char> ComputeAlignedContentBuffer(Span2D<char> buffer)
-    {
-        if (Content is null) return buffer;
-
-        var maxSize = buffer.BlockSize();
-        var contentSize = Content.CalcSize(maxSize).Constrain(maxSize);
-
-        var hDimension = ComputeAlignedDimension(maxSize.Width, contentSize.Width, HorizontalContentAlignment);
-        var vDimension = ComputeAlignedDimension(maxSize.Height, contentSize.Height, VerticalContentAlignment);
-
-        return buffer.Slice(
-            row: vDimension.start,
-            column: hDimension.start,
-            height: vDimension.length,
-            width: hDimension.length
-        );
-    }
-
-    /// <summary>Calculates a slice of the full length of a single dimension that the content should be rendered to.</summary>
-    private static (int start, int length) ComputeAlignedDimension(int maxSize, int desiredSize, Alignment alignment)
-    {
-        var sanitizedSize = Math.Min(maxSize, desiredSize);
-
-        switch (alignment)
-        {
-            case Alignment.Start: return (0, sanitizedSize);
-            case Alignment.Center:
-                var margin = (maxSize - sanitizedSize) / 2;
-                var length = sanitizedSize;
-                return (margin, length);
-            case Alignment.End: return (maxSize - sanitizedSize, sanitizedSize);
-            default: throw new ArgumentOutOfRangeException(nameof(alignment), alignment, "Unknown alignment type");
-        }
     }
 }
