5619606275e60670398dce35a692545a5ed9af1c
Cole Schultz <schultzcole@gmail.com>
2023-02-23 22:06:01 -0600

    Add FixedSizeBlock

---
+++ src/FlexBlocks/Blocks/FixedSizeBlock.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.HighPerformance;
+using FlexBlocks.BlockProperties;
+
+namespace FlexBlocks.Blocks;
+
+/// <summary>
+/// A block that always renders as the same size, regardless of its content
+/// </summary>
+public class FixedSizeBlock : UiBlock
+{
+    public UiBlock? Content { get; set; }
+
+    /// <summary>The fixed size of this block.</summary>
+    public UnboundedBlockSize Size { get; set; }
+
+    /// <summary>The desired width of this block.</summary>
+    public BlockLength Width
+    {
+        get => Size.Width;
+        set => Size = Size with { Width = value };
+    }
+
+    /// <summary>The desired height of this block.</summary>
+    public BlockLength Height
+    {
+        get => Size.Height;
+        set => Size = Size with { Height = value };
+    }
+
+    /// <inheritdoc />
+    public override UnboundedBlockSize CalcMaxSize() => Size;
+
+    /// <inheritdoc />
+    public override BlockSize CalcSize(BlockSize maxSize) => maxSize.Constrain(Size);
+
+    /// <inheritdoc />
+    public override void Render(Span2D<char> buffer)
+    {
+        if (Content is null) return;
+        RenderChild(Content, buffer);
+    }
+
+    /// <inheritdoc />
+    public override RerenderMode GetRerenderModeForChild(UiBlock child) => RerenderMode.DesiredSizeChanged;
+}
