3dd38aed29df4e58e6bcd7434cdcfc7ea63cfdfd
Cole Schultz <schultzcole@gmail.com>
2023-04-08 18:48:27 -0500

    add test DummyBlock for testing presence of blocks

---
+++ test/FlexBlocksTest/Utils/DummyBlock.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.HighPerformance;
+using FlexBlocks.BlockProperties;
+using FlexBlocks.Blocks;
+
+namespace FlexBlocksTest.Utils;
+
+/// <summary>Doesn't do anything</summary>
+public class DummyBlock : UiBlock
+{
+    public int Id { get; }
+    public DummyBlock(int id) { Id = id; }
+
+    public override UnboundedBlockSize CalcMaxSize() => throw new System.NotImplementedException();
+    public override BlockSize CalcSize(BlockSize maxSize) => throw new System.NotImplementedException();
+    public override void Render(Span2D<char> buffer) => throw new System.NotImplementedException();
+}
